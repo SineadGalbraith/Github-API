81ed18f47a64f83ff9bac8eda1d6866525be1a93
Sinéad Galbraith <SineadGalbraith@users.noreply.github.com>
2018-11-09 12:44:43 +0000

    Basic Error Handling Added

--- GithubAPIRetrieval.py
+++ GithubAPIRetrieval.py
@@ -1,16 +1,29 @@
 from github import Github
-
+print('This program will return details on the user entered.')
 g = Github()
 term = 'y'
-
+i = 1
 while(term == 'y'):
+    i = 1
     user = input('\nPlease enter your username: ')
 
     userDetails = g.get_user(user)
     print('Name: ' + userDetails.name)
+    print('Followers: ' + str(userDetails.followers))
+    print('Following: ' + str(userDetails.following))
+    print('Public Repositories: ' + str(userDetails.public_repos))
+    print('Private Repositories: ' + str(userDetails.total_private_repos))
     
-    term = input('\nCheck another user (y/n):')
-    if(term == 'n'):
-        break;
-    else:
-        term = 'y'
+    while(i == 1):
+        term = input('\nCheck another user (y/n): ')
+        if(term == 'n'):
+            break
+            i = 0
+        elif(term == 'y'):
+            term = 'y'
+            i = 0
+        else:
+            print('Error.')
+            i = 1
+        
+            
